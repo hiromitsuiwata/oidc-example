7f211bc38896b906e42105149cdb740e14b9bbdb
Hiromitsu Iwata <hiromitsu7@gmail.com>
2022-08-24 00:12:19 +0900

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # oidc-example
+
+http://localhost:9080/oidc-example/api/properties
+
+http://localhost:8080/auth/
---
+++ webapp/src/main/java/com/example/ExampleApplication.java
@@ -0,0 +1,10 @@
+package com.example;
+
+import jakarta.ws.rs.core.Application;
+import jakarta.ws.rs.ApplicationPath;
+
+
+@ApplicationPath("/api")
+public class ExampleApplication extends Application {
+    
+}
