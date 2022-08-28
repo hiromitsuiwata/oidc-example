381b235fe2c0834e33b087925fbd812eb0ff40c9
Hiromitsu Iwata <hiromitsu7@gmail.com>
2022-08-28 10:35:49 +0900

    jwtを取得

--- myclient/src/main/java/com/example/PropertiesResource.java
+++ myclient/src/main/java/com/example/PropertiesResource.java
@@ -1,19 +1,26 @@
 package com.example;
 
 import jakarta.enterprise.context.RequestScoped;
+import jakarta.inject.Inject;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.core.MediaType;
+import org.eclipse.microprofile.jwt.JsonWebToken;
 
 @RequestScoped
 @Path("/properties")
 public class PropertiesResource {
 
+	@Inject
+	private JsonWebToken jwt;
+
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response getProperties() {
-        return Response.ok(System.getProperties()).build();
+		System.out.println(jwt);
+
+		return Response.ok(System.getProperties().toString() + jwt.toString()).build();
 	}
 }
